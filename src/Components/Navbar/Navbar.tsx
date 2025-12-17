e5a152f37ce0b0065b6f4fdcbd23f03578d10428
Lukas <lukas.patrikc@yahoo.com>
2025-12-17 21:10:08 +0200

    Navbar fully functional

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,20 +1,54 @@
+import { useState } from "react";
 import "./Navbar.css";
+import { useNavigate } from "react-router";
 
 export default function Navbar() {
+  const [isClicked, setIsClicked] = useState(false);
+  const navigate = useNavigate();
+
+  function toProfile() {
+    navigate("/Profile");
+  }
+
+  function logOut() {
+    navigate("/Login");
+    localStorage.removeItem("token");
+  }
+
+  function Home() {
+    navigate("/LandingPage");
+  }
+
   return (
     <div className="nav">
       <div className="name">
         <p>WealthGrow</p>
       </div>
-      <div className="links">
-        <ul>
-          <li>Home</li>
-          <li>Wallet</li>
-          <li>Your chart</li>
-        </ul>
-      </div>
-      <div>
-        <img className="profileImg" src="/poza_cv.jpg" alt="" />
+
+      <ul className="links">
+        <li onClick={Home}>Home</li>
+        <li>Wallet</li>
+        <li>Your chart</li>
+      </ul>
+
+      <div className="profileWrapper">
+        <img
+          onClick={() => setIsClicked(!isClicked)}
+          className="profileImg"
+          src="/poza_cv.jpg"
+          alt="profile"
+        />
+
+        {isClicked && (
+          <ul className="dropdown">
+            <li onClick={toProfile} className="dropdownLink">
+              Profile
+            </li>
+            <li onClick={logOut} className="dropdownLink">
+              Sign Out
+            </li>
+          </ul>
+        )}
       </div>
     </div>
   );
