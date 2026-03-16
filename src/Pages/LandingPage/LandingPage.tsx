e7b83800ee90f5882b7ba7840bd9263b529c1a29
Lukas <lukas.patrikc@yahoo.com>
2026-03-16 23:32:59 +0200

    main page design remade

--- src/Pages/LandingPage/LandingPage.tsx
+++ src/Pages/LandingPage/LandingPage.tsx
@@ -10,44 +10,38 @@ import Portofolio from "../../Services/PortofolioService";
 import type { total } from "../../Services/Interfaces/TotalInterface";
 import SockJS from "sockjs-client";
 import Stomp from "stompjs";
-import authService from "../../Services/UserService";
-import type { User } from "../../Services/Interfaces/UserInterface";
-import { RiWallet3Line } from "react-icons/ri";
+// import authService from "../../Services/UserService";
+// import type { User } from "../../Services/Interfaces/UserInterface";
+
 import type { DailyProfit } from "../../Services/Interfaces/ProfitInterface";
-import { MdOutlineTrendingUp } from "react-icons/md";
-import { PiPiggyBank } from "react-icons/pi";
-import { IoIosCheckmarkCircle } from "react-icons/io";
-import { FaArrowTrendDown } from "react-icons/fa6";
+
 import { FaPlusCircle } from "react-icons/fa";
 import { FaMinusCircle } from "react-icons/fa";
-import AllocationVisualizer from "../../Components/Reuseable/AllocationVisualizer.tsx/AllocationVisualizer";
 
 export default function LandingPage() {
   const [stock, setStock] = useState<Stock[]>();
   const [name, setName] = useState<string>("AAPL");
   const [price, setPrice] = useState<number>();
-  const [status, setStatus] = useState<string>();
   const [closedBuy, setClosedBuy] = useState<boolean>(false);
   const [closedSell, setClosedSell] = useState<boolean>(false);
   const [portofolio, setPortofolio] = useState<portofolioData>();
   const [total, setTotal] = useState<total>();
-  const [user, setUser] = useState<User>();
+  // const [user, setUser] = useState<User>();
   const [profit, setProfit] = useState<DailyProfit>();
 
-  const getUser = async () => {
-    try {
-      const response = await authService.getUser();
-      setUser(response);
-      console.log(response);
-    } catch (e) {
-      console.log(e);
-    }
-  };
+  // const getUser = async () => {
+  //   try {
+  //     const response = await authService.getUser();
+  //     setUser(response);
+  //     console.log(response);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
 
-  const getStockInfo = (symbol: string, price: number, status: string) => {
+  const getStockInfo = (symbol: string, price: number) => {
     setName(symbol);
     setPrice(price);
-    setStatus(status);
   };
 
   const fetchData = useCallback(async () => {
@@ -81,7 +75,7 @@ export default function LandingPage() {
 
   useEffect(() => {
     setTimeout(() => {
-      getUser();
+      // getUser();
       fetchData().catch((e) => console.log(e));
     }, 0);
   }, [fetchData]);
@@ -113,76 +107,6 @@ export default function LandingPage() {
 
   return (
     <div className="mainLanding">
-      <div className="title">
-        <h1>Dashboard</h1>
-        <p>
-          Welcome back, {user?.firstName}. Here's your portfolio performance
-        </p>
-      </div>
-      <div className="containers">
-        <div className="cont">
-          <div className="topCont">
-            <div className="ContIcon">
-              <RiWallet3Line />
-            </div>
-            <p
-              className="profitCont"
-              style={
-                (profit?.totalPortfolioProfitDailyPercentage ?? 0) > 0
-                  ? { background: "#00ff00", color: "#000000" }
-                  : { background: "#ff0000", color: "#ffffff" }
-              }
-            >
-              {profit?.totalPortfolioProfitDailyPercentage.toFixed(2)}%
-            </p>
-          </div>
-          <div className="mainCont">
-            <p className="details">Total Portofolio Value</p>
-            <h2 className="sum">$ {total?.total}</h2>
-          </div>
-        </div>
-        <div className="cont">
-          <div className="topCont">
-            <div className="ContIcon">
-              <MdOutlineTrendingUp />
-            </div>
-            {(profit?.dailyProfitPercentage ?? 0) > 0 ? (
-              <p
-                className="profitCont"
-                style={{
-                  background: "#00ff00",
-                  color: "#000000",
-                }}
-              >
-                &uarr; High
-              </p>
-            ) : (
-              <p
-                className="profitCont"
-                style={{ background: "#ff0000", color: "#ffffff" }}
-              >
-                &darr; Low
-              </p>
-            )}
-          </div>
-          <div className="mainCont">
-            <p className="details">Total gain/Loss</p>
-            <h2 className="sum">$ {profit?.dailyProfitValue.toFixed(2)}</h2>
-          </div>
-        </div>
-        <div className="cont">
-          <div className="topCont">
-            <div className="ContIcon">
-              <PiPiggyBank />
-            </div>
-            <p className="profitCont"></p>
-          </div>
-          <div className="mainCont">
-            <p className="details">Annual Dividend</p>
-            <h2 className="sum">$ 560</h2>
-          </div>
-        </div>
-      </div>
       {closedBuy && (
         <BuyPopUp
           Symbol={name}
@@ -198,122 +122,137 @@ export default function LandingPage() {
         ></SellPopUp>
       )}
 
-      <div className="displayContainer">
-        <div className="stockHeader"></div>
-        <div className="charts">
-          <div className="graph">
-            <div className="chartBottom">
-              <div className="stockStatus">
-                <div className="stat">
-                  <h1 className="displayTitle">{name}</h1>
-                  <div className="status">
-                    <IoIosCheckmarkCircle /> {status}
-                  </div>
-                </div>
-                <div className="prices">
-                  <h1 className="price"> ${price?.toFixed(2)}</h1>
-                  {(portofolio?.profit ?? 0) < 0 ? (
-                    <div className="lossSec">
-                      <FaArrowTrendDown />
-                      <p className="profit">${portofolio?.profit.toFixed(2)}</p>
-                    </div>
-                  ) : (
-                    <div className="profitSec">
-                      <MdOutlineTrendingUp />
-                      <p className="profit">
-                        $+{portofolio?.profit.toFixed(2)}
-                      </p>
-                    </div>
-                  )}
+      <div className="mainCont">
+        <div className="stockList">
+          <div className="stockTitle">
+            <h3>Stocks</h3>
+            <p style={{ color: "#136dec" }}>View All</p>
+          </div>
+          {stock?.map((stock) => {
+            return (
+              <div
+                key={stock.symbol}
+                className={`stock ${stock.symbol === name ? "selected" : ""}`}
+                onClick={() => {
+                  getStockInfo(stock.symbol, stock.price);
+                }}
+              >
+                <h4 className="stockName">{stock.symbol}</h4>
+                <div className="pricePlace">
+                  <p className="price">{stock.price.toFixed(2)} $</p>
                 </div>
               </div>
-              <div className="btnSection">
-                <button
-                  style={{ background: "#136dec" }}
-                  onClick={() => setClosedBuy(true)}
-                  className="bBtn"
-                >
-                  <FaPlusCircle /> buy
-                </button>
-                <button
-                  style={{ background: "#F1F5F9", color: "#000" }}
-                  onClick={() => setClosedSell(true)}
-                  className="bBtn"
-                >
-                  <FaMinusCircle /> Sell
-                </button>
+            );
+          })}
+        </div>
+      </div>
+      <div className="chartContaier">
+        <Charts name={name}></Charts>
+        <div className="stats">
+          <div className="stockStats">
+            <h4 style={{ color: "#6B7280", padding: "1.3rem  1.3rem  0" }}>
+              ACCOUNT AT A GLAZE
+            </h4>
+            <div className="moneySection">
+              <div className="moneyContainer">
+                <div className="monCont">
+                  <p className="MSTitle">Total Profit</p>
+                  <h3
+                    style={
+                      (portofolio?.profit ?? 0) > 0
+                        ? { color: "#10B981" }
+                        : { color: "red" }
+                    }
+                    className="MSMoney"
+                  >
+                    {(portofolio?.profit ?? 0) > 0
+                      ? `+${portofolio?.profit.toFixed(2)}`
+                      : portofolio?.profit.toFixed(2)}
+                  </h3>
+                </div>
+                <div className="monCont">
+                  <p className="MSTitle">Total Owned</p>
+                  <h3 className="MSMoney">{portofolio?.amountOwned}</h3>
+                </div>
+              </div>
+              <div className="stockNeeds">
+                <p>Average Price</p>
+                <p>{portofolio?.averagePrice}</p>
+              </div>
+              <div className="stockNeeds">
+                <p>Shares Owned</p>
+                <p>{portofolio?.shares}</p>
+              </div>
+              <div className="stockNeeds">
+                <p>Portfolio %</p>
+                <p>{portofolio?.averagePrice}</p>
               </div>
             </div>
-            <Charts name={name}></Charts>
-            <div className="profitContainers">
-              <div className="profContainer">
-                <p className="profContTitle">Shares Owened</p>
-                <h1 className="profContPrice">
-                  {portofolio?.shares.toFixed(2)}
-                </h1>
-              </div>
-              <div className="profContainer">
-                <p className="profContTitle">Average Price</p>
-                <h1 className="profContPrice">
-                  ${portofolio?.averagePrice.toFixed(2)}
-                </h1>
-              </div>
-              <div className="profContainer">
-                <p className="profContTitle">Average Price</p>
-                <h1 className="profContPrice">
-                  ${portofolio?.averagePrice.toFixed(2)}
-                </h1>
-              </div>
-              <div className="profContainer">
-                <p className="profContTitle">Average Price</p>
-                <h1 className="profContPrice">
-                  ${portofolio?.averagePrice.toFixed(2)}
-                </h1>
-              </div>
+            <div className="btnSection">
+              <button
+                style={{ background: "#136dec" }}
+                onClick={() => setClosedBuy(true)}
+                className="bBtn"
+              >
+                <FaPlusCircle /> buy
+              </button>
+              <button
+                style={{ background: "#F1F5F9", color: "#000" }}
+                onClick={() => setClosedSell(true)}
+                className="bBtn"
+              >
+                <FaMinusCircle /> Sell
+              </button>
             </div>
           </div>
-        </div>
-        <div className="stockContainer">
-          <div className="stockContainerHelper">
-            <div className="topStockContainer">
-              <h3 className="stockTitle">Top Holders</h3>
-              <p style={{ cursor: "pointer" }} className="seeAll">
-                View All &rarr;
-              </p>
+          <div className="history">
+            <h4 style={{ color: "#6B7280", padding: "1.3rem  1.3rem  0" }}>
+              ACCOUNT AT A GLAZE
+            </h4>
+            <div className="containerGrid">
+              <div className="gridHelper">
+                <div className="histContainers">
+                  <p className="histTitle">Total Portfolio</p>
+                  <h3 className="histAmount">${total?.total}</h3>
+                </div>
+                <div className="histContainers">
+                  <p className="histTitle">Total Profit</p>
+                  <h3
+                    style={
+                      (profit?.totalPortfolioProfitDaily ?? 0) > 0
+                        ? { color: "#10B981" }
+                        : { color: "red" }
+                    }
+                    className="histAmount "
+                  >
+                    {(profit?.totalPortfolioProfitDaily ?? 0) > 0
+                      ? `+${profit?.totalPortfolioProfitDaily.toFixed(2)} $`
+                      : profit?.totalPortfolioProfitDaily.toFixed(2)}
+                  </h3>
+                </div>
+              </div>
+              <div className="gridHelper">
+                <div className="histContainers">
+                  <p className="histTitle">Total Portfolio</p>
+                  <h3 className="histAmount">${total?.total}</h3>
+                </div>
+                <div className="histContainers">
+                  <p className="histTitle">Total Profit</p>
+                  <h3
+                    style={
+                      (profit?.totalPortfolioProfitDaily ?? 0) > 0
+                        ? { color: "#10B981" }
+                        : { color: "red" }
+                    }
+                    className="histAmount "
+                  >
+                    {(profit?.totalPortfolioProfitDaily ?? 0) > 0
+                      ? `+${profit?.totalPortfolioProfitDaily.toFixed(2)} $`
+                      : profit?.totalPortfolioProfitDaily.toFixed(2)}
+                  </h3>
+                </div>
+              </div>
             </div>
-            <ul className="listTitles">
-              <li>ASSET</li>
-              <li>PRICE</li>
-              <li>LOW</li>
-              <li>HIGH</li>
-              <li className="allocationLi">ALLOCATION</li>
-              <li>VALUE</li>
-            </ul>
-            {stock?.map((stock) => {
-              const charPercentage = total?.portfolioChart.find(
-                (chart) => chart.symbol === stock.symbol,
-              );
-              return (
-                <ul
-                  key={stock.symbol}
-                  onClick={() =>
-                    getStockInfo(stock.symbol, stock.price, stock.status)
-                  }
-                  className="stocksUl"
-                >
-                  <li className="stockLi">{stock.symbol}</li>
-                  <li className="stockLi">${stock.price} $</li>
-                  <li className="stockLi">${stock.low} $</li>
-                  <li className="stockLi">${stock.high} $</li>
-                  <li className="allocationLi">
-                    <AllocationVisualizer
-                      percentage={charPercentage?.percentage ?? 0}
-                    ></AllocationVisualizer>
-                  </li>
-                  <li className="stockLi">{portofolio?.amountOwned} $</li>
-                </ul>
-              );
-            })}
           </div>
         </div>
       </div>
